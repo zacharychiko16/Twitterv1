803f0888894a768826cb6959fde131bf85116677
zacharychiko16 <zacharychiko16@gmail.com>
2023-01-29 11:20:42 -0400

    search bar completed

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import Sidebar from "@/components/Sidebar";
 import Feed from "@/components/Feed";
+import Widgets from "@/components/Widgets";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function Home() {
       <main className="flex min-h-screen max-w-7xl mx-auto">
         <Sidebar />
         <Feed />
+        <Widgets />
       </main>
     </div>
   );
