ca4caaf42ccfa952ab22887abe757d1c954ef8dd
zacharychiko16 <zacharychiko16@gmail.com>
2023-01-29 21:03:56 -0400

    created news section of widegts component

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,9 @@
 import { SearchIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+import News from "./News";
 
-export default function Widgets() {
+export default function Widgets({newsResults}) {
+    const [articleNum, setArticleNum] = useState(3)
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
@@ -13,6 +16,15 @@ export default function Widgets() {
           />
         </div>
       </div>
+
+      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+        <h4 className="font-bold text-xl px-4">Whats's happening</h4>
+        {newsResults.slice(0,articleNum).map((article)=> (
+        <News key={article.title} article={article}/>
+        ))}
+        <button onClick={() => setArticleNum(articleNum + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show more</button>
+      </div>
+    
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Widgets from "@/components/Widgets";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({newsResults}) {
   return (
     <div>
       <Head>
@@ -16,11 +16,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen max-w-7xl mx-auto">
+      <main className="flex min-h-screen mx-auto">
         <Sidebar />
         <Feed />
-        <Widgets />
+        <Widgets newsResults={newsResults.articles} />
       </main>
     </div>
   );
+}
+
+// https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
+
+export async function getServerSideProps() {
+  const newsResults = await fetch(
+    "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+  ).then((res) => res.json());
+  return {
+    props: {
+      newsResults,
+    },
+  };
 }
